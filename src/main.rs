17d525db3937fd4985118d1f71993c0539dee79f
Noah Lidell <noahlidell@gmail.com>
2019-05-05 03:54:43 -0500

    one step after working black screen

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,29 @@ impl SimpleState for Pong {
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
+    use amethyst::utils::application_root_dir;
+    let app_root = application_root_dir();
+    dbg!(&app_root);
+    //let path = app_root.join("resources/display_config.ron");
+    let path = app_root+"resources/display_config.ron";
+    dbg!(&path);
+    let config = DisplayConfig::load(&path);
+    let pipe = Pipeline::build()
+        .with_stage(
+            Stage::with_backbuffer()
+                .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
+                .with_pass(DrawFlat2D::new()),
+        );
+    
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            RenderBundle::new(pipe, Some(config))
+                .with_sprite_sheet_processor()
+        )?;
+
+    let mut game = Application::new("./", Pong, game_data)?;
+
+    game.run();
+
     Ok(())
 }
